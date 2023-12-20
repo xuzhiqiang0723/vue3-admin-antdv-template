114ccd124924cec6bfd3bb1b627b4ec98d23b5a7
xzq <xzq710675281@163.com>
2023-12-20 15:27:15 +0800

    feat:eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,31 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index'],
+      },
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        semi: false,
+        tabWidth: 2,
+        singleQuote: true,
+        printWidth: 100,
+        trailingComma: 'all',
+        endOfLine: 'auto',
+      },
+    ],
+  },
 }
