e77d45b2e4d02cb7866787900147a27eb6634399
xzq <xzq710675281@163.com>
2023-12-21 11:15:09 +0800

    feat:theme and locale

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+const theme = {
+  token: {
+    colorPrimary: '#e15536',
+  },
+}
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
+</script>
 
 <template>
-  <a-button ghost type="primary"><HomeOutlined />主色按钮</a-button>
+  <a-config-provider :theme="theme" :locale="zhCN">
+    <a-button type="primary"><HomeOutlined />主色按钮</a-button>
+    <a-pagination :total="500" />
+    <a href="#">hello World</a>
+  </a-config-provider>
 </template>
 
 <style scoped></style>
