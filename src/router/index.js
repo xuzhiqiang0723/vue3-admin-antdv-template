6ddf960f64e15a990bd7717393e74099d7bebeaa
xzq <xzq710675281@163.com>
2023-12-21 13:48:39 +0800

    feat:NProgress and env

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({
+  showSpinner: false,
+})
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -6,14 +12,33 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      meta: {
+        title: '首页',
+      },
       component: () => import('@/views/Layout/index.vue'),
     },
     {
       path: '/login',
       name: 'login',
+      meta: {
+        title: '登录页',
+      },
       component: () => import('@/views/Login/index.vue'),
     },
   ],
+})
+
+router.beforeEach(() => {
+  // 进度条开始
+  NProgress.start()
+})
+
+// 全局的后置导航
+router.afterEach((to) => {
+  // 进度条结束
+  NProgress.done()
+  // 动态设置标题
+  document.title = `${to.meta.title || import.meta.env.VITE_APP_TITLE}`
 })
 
 export default router
